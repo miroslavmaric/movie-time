2aca60db9589e39e6f3d846cf5e10c29b323765d
Miroslav Maric <miroslav.feniks@gmail.com>
2016-02-03 22:03:59 +0300

    Add ListView to the activity and display test data.

--- app/src/main/java/com/nightsparrow/movietime/MainActivityFragment.java
+++ app/src/main/java/com/nightsparrow/movietime/MainActivityFragment.java
@@ -1,15 +1,22 @@
 package com.nightsparrow.movietime;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    public ArrayAdapter<String> mMovieAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +24,27 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        String[] data = {
+                "Tommorow - 60",
+                "Wasteland - 210",
+                "Nimmerland - 120",
+                "Drake LAke - 140"
+        };
+
+        ArrayList<String> dataList = new ArrayList<>(Arrays.asList(data));
+
+        mMovieAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                R.layout.list_item_movie,
+                R.id.list_item_movie_textview,
+                dataList
+        );
+
+        ListView listview = (ListView) root.findViewById(R.id.listview_movie);
+        listview.setAdapter(mMovieAdapter);
+
+        return root;
     }
 }
