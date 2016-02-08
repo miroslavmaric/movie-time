2c2f264788bcda897489182bb012f65acb3073d1
Miroslav Maric <miroslav.feniks@gmail.com>
2016-02-09 01:32:01 +0300

    Prepare test classes.

---
+++ app/src/androidTest/java/com/nightsparrow/movietime/FullTestSuite.java
@@ -0,0 +1,21 @@
+package com.nightsparrow.movietime;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Created by x on 2/8/2016.
+ */
+public class FullTestSuite extends TestSuite {
+
+    public FullTestSuite() {
+        super();
+    }
+
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class)
+                .includeAllPackagesUnderHere().build();
+    }
+}
---
+++ app/src/androidTest/java/com/nightsparrow/movietime/data/TestDb.java
@@ -0,0 +1,27 @@
+package com.nightsparrow.movietime.data;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Miroslav Maric on 2/9/2016.
+ */
+public class TestDb extends AndroidTestCase {
+
+    public static final String LOG_TAG = TestDb.class.getSimpleName();
+
+
+
+    public void setUp() {
+
+    }
+
+    void deleteDatabase() {
+
+    }
+
+    public long insertMovie() {
+        return -1;
+    }
+
+
+}
