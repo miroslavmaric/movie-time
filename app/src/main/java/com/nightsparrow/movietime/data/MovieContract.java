c454d5acdc4308a34c7a879998f14341c8cb381e
Miroslav Maric <miroslav.feniks@gmail.com>
2016-02-11 01:58:04 +0300

    Create MovieContract.

---
+++ app/src/main/java/com/nightsparrow/movietime/data/MovieContract.java
@@ -0,0 +1,12 @@
+package com.nightsparrow.movietime.data;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Miroslav Maric on 2/11/2016.
+ */
+public class MovieContract implements BaseColumns {
+    public static final String TABLE_NAME = "movie";
+
+    public static final String COLUMN_ORIGINAL_TITLE = "original_title";
+}
