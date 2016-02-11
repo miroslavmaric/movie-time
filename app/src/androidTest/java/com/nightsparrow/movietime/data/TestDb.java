bfea79544856c4d7060ff8fd141cb2c93e3690dd
Miroslav Maric <miroslav.feniks@gmail.com>
2016-02-11 18:26:29 +0300

    Test insertion into the database.

--- app/src/androidTest/java/com/nightsparrow/movietime/data/TestDb.java
+++ app/src/androidTest/java/com/nightsparrow/movietime/data/TestDb.java
@@ -7,6 +7,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.test.AndroidTestCase;
 
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  * Created by Miroslav Maric on 2/9/2016.
@@ -109,6 +110,19 @@ public class TestDb extends AndroidTestCase {
         return testValues;
     }
 
+    static void validateCurrentRecord(String error, Cursor valueCursor, ContentValues expectedValues) {
+
+        for (Map.Entry<String, Object> entry : expectedValues.valueSet()) {
+            String columnName = entry.getKey();
+            int idx = valueCursor.getColumnIndex(columnName);
+            assertFalse("Column '" + columnName + "' not found. " + error, idx == -1);
+            String expectedValue = entry.getValue().toString();
+            assertEquals("Value '" + entry.getValue().toString() +
+                    "' did not match the expected value '" +
+                    expectedValue + "'. " + error, expectedValue, valueCursor.getString(idx));
+        }
+    }
+
     static long insertImaginaryMovieValues(Context context) {
         // insert our test records into the database
         MovieDbHelper dbHelper = new MovieDbHelper(context);
@@ -122,6 +136,54 @@ public class TestDb extends AndroidTestCase {
         assertTrue("Error: Failure to insert Imaginary Movie Values", locationRowId != -1);
 
         return locationRowId;
+    }
+
+    public void testMovieTable() {
+        // 1. Get a reference to the writable database.
+        MovieDbHelper dbHelper = new MovieDbHelper(mContext);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+        // 2. Create values for the table
+        ContentValues testValues = TestDb.createImaginaryMovieValues();
+
+        // 3. Insert ContentValues into database and get a row ID back
+        long locationRowId;
+        locationRowId = db.insert(MovieContract.MovieEntry.TABLE_NAME, null, testValues);
+
+        // Verify we got a row back.
+        assertTrue(locationRowId != -1);
+
+        // Data's inserted.  IN THEORY.  Now pull some out to stare at it and verify it made
+        // the round trip.
+
+        // 4. Query the database and receive a Cursor back
+        // A cursor is a primary interface to the query results.
+        Cursor cursor = db.query(
+                MovieContract.MovieEntry.TABLE_NAME,  // Table to Query
+                null, // all columns
+                null, // Columns for the "where" clause
+                null, // Values for the "where" clause
+                null, // columns to group by
+                null, // columns to filter by row groups
+                null // sort order
+        );
+
+        // Move the cursor to a valid database row and check to see if we got any records back
+        // from the query
+        assertTrue("Error: No Records returned from movie query", cursor.moveToFirst());
+
+        // 5. Validate data in resulting Cursor with the original ContentValues
+        TestDb.validateCurrentRecord("Error: Movie Query Validation Failed",
+                cursor, testValues);
+
+        // Finally, close the cursor and database
+        // Move the cursor to demonstrate that there is only one record in the database
+        assertFalse("Error: More than one record returned from location query",
+                cursor.moveToNext());
+
+        // Sixth Step: Close Cursor and Database
+        cursor.close();
+        db.close();
     }
 
 }
