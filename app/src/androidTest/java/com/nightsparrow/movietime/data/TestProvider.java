5637d375bdb8688a1fc4a45fb8c51b69f3d2730a
Miroslav Maric <miroslav.feniks@gmail.com>
2016-02-11 21:45:57 +0300

    Register content provider.

---
+++ app/src/androidTest/java/com/nightsparrow/movietime/data/TestProvider.java
@@ -0,0 +1,38 @@
+package com.nightsparrow.movietime.data;
+
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.ProviderInfo;
+import android.test.AndroidTestCase;
+
+/**
+ * Created by Miroslav Maric on 2/11/2016.
+ */
+public class TestProvider extends AndroidTestCase {
+
+    public static final String LOG_TAG = TestProvider.class.getSimpleName();
+
+    public void testProviderRegistry() {
+        PackageManager pm = mContext.getPackageManager();
+
+        // We define the component name based on the package name from the context and the
+        // WeatherProvider class.
+        ComponentName componentName = new ComponentName(mContext.getPackageName(),
+                MovieProvider.class.getName());
+        try {
+            // Fetch the provider info using the component name from the PackageManager
+            // This throws an exception if the provider isn't registered.
+            ProviderInfo providerInfo = pm.getProviderInfo(componentName, 0);
+
+            // Make sure that the registered authority matches the authority from the Contract.
+            assertEquals("Error: MovieProvider registered with authority: " + providerInfo.authority +
+                            " instead of authority: " + MovieContract.CONTENT_AUTHORITY,
+                    providerInfo.authority, MovieContract.CONTENT_AUTHORITY);
+        } catch (PackageManager.NameNotFoundException e) {
+            // I guess the provider isn't registered correctly.
+            assertTrue("Error: MovieProvider not registered at " + mContext.getPackageName(),
+                    false);
+        }
+    }
+
+}
